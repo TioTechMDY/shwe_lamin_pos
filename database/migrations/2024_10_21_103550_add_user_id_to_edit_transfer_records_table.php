ccb44c0fd855b05e7e0822bc4d811a0e2283b7cc
Pyae Sone Phyoe <53340902+pyaesonephyo527@users.noreply.github.com>
2024-10-21 12:10:29 +0630

    Admin Id is Integer

--- database/migrations/2024_10_21_103550_add_user_id_to_edit_transfer_records_table.php
+++ database/migrations/2024_10_21_103550_add_user_id_to_edit_transfer_records_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('edit_transfer_records', function (Blueprint $table) {
-            $table->int('admin_id')->after('new_quantity');
+            $table->integer('admin_id')->after('new_quantity');
         });
     }
 
--- database/migrations/2024_10_21_103824_add_user_id_to_edit_transaction_news_table.php
+++ database/migrations/2024_10_21_103824_add_user_id_to_edit_transaction_news_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('edit_transaction_news', function (Blueprint $table) {
-            $table->int('admin_id')->after('new_quantity');
+            $table->integer('admin_id')->after('new_quantity');
         });
     }
 
