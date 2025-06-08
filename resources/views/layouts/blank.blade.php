80ade62d716d1667c7f1e0d9a285e345cb3e0a57
Pyae Sone Phyoe <53340902+PyaeSonePhyoe4545@users.noreply.github.com>
2025-06-09 01:59:53 +0700

    Update blank.blade.php

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="shortcut icon" href="{{asset('public/assets/installation')}}/assets/img/favicon.svg">
+    <link rel="shortcut icon" href="{{asset('public/assets/installation/assets/img/favicon.svg')}}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{asset('public/assets/installation')}}/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{asset('public/assets/installation')}}/assets/css/style.css">
+    <link rel="stylesheet" href="{{asset('public/assets/installation/assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/assets/installationassets/css/style.css')}}/">
 </head>
 
 <body>
